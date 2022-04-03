e73d8496ec2deb8ed2ae904e4a6fceee91dd095c
Shupa-Alam <99464387+Shupa-Alam@users.noreply.github.com>
2022-04-02 23:29:49 -0500

    practice

--- src/test/java/steps/LoginStepDefinition.java
+++ src/test/java/steps/LoginStepDefinition.java
@@ -1,13 +1,20 @@
 package steps;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 
 public class LoginStepDefinition {
 
 	//Given user is on Techfios Login page 	
 	
 	@Given ("^user is on Techfios Login page$")
-	public void user_is_on_techfios_login_page1() {
-	
-}
+	public void user_is_on_techfios_login_page() {
+}		
+//When user enter username as "demo@techfios.com"
+
+ @When ("^user enter username as \"([^\"]*)\"$")
+ public void user_enters_usersname_as(String username) {
+	 
+ }
+ 
 }
